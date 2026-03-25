1c5b873de6520ceee369f6c5c90bc6119b4d8fb5
Andrey Abramov <i.andrey.abramov@gmail.com>
2026-03-25 23:53:02 +0300

    feat: replace next.js with astro

--- apps/landing/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-svh flex-col items-center justify-center gap-4">
-      <h1>Daylist</h1>
-      <p>Next.js web app is running.</p>
-    </main>
-  );
-}
